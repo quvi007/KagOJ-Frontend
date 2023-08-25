e4f0e0b5fec73b2e6454879088aa4602820cfbba
quvi007 <1805007@ugrad.cse.buet.ac.bd>
2023-08-25 20:36:24 +0600

    Add existing project files to Git

--- KagOJ-Frontend/src/routes/semesterDelete.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteSemester } from "../semesters";
-
-export async function action({ params }) {
-    await deleteSemester(params.semesterId);
-    return redirect("/semesters");
-}
